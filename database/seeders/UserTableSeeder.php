b74dfb0995a7e4751baefac4f57d2f746ace495c
Faisal Ahmmed <86137950+faisalahmmedapon@users.noreply.github.com>
2022-11-24 20:36:16 +0600

    chnage

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Hash;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
